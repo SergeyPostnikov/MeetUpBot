bb9d048539eb3c7857e8f2f034ae635f982bd382
Flash-kir <flash-kir@yandex.ru>
2023-06-22 15:21:26 +0300

    add bot_test command to .gitignore, import models to bot.py

--- meetup/meetup_bot/management/commands/bot.py
+++ meetup/meetup_bot/management/commands/bot.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from meetup_bot.models import Meetup, MemberStatus, Member
 
 
 class Command(BaseCommand):
     help = 'Запуск чат-бота'
 
     def handle(self, *args, **options):
-        # переменные импортируем с файла настроек: settings.TG_BOT_KEY
         pass
