eb66380ae103cb54dedb4f5e589c1f03f764b656
Sergey Postnikov <ser.post2017@gmail.com>
2023-06-24 15:10:14 +0200

    setup wsgi.py

--- meetup/wsgi.py
+++ meetup/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+from meetup.settings import env, BASE_DIR
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meetup.settings')
+path = env('PROJECT_ROOT', default=BASE_DIR)
+
+if path not in sys.path:
+   sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'meetup.settings'
 
 application = get_wsgi_application()
